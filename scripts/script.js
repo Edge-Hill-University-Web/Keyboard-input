48253184b2847498d244863f14e7adf50231bb64
Danny2097 <danielcampbell2097@hotmail.com>
2023-10-11 12:50:45 +0100

    upload

---
+++ scripts/script.js
@@ -0,0 +1,66 @@
+let player;
+let cursors;
+let canvas;
+
+class KeyboardExample extends Phaser.Scene {
+
+    
+    preload() {
+        this.load.image('ship', 'assets/rocket.png');
+        this.load.image('background', 'assets/background.png');
+    }
+
+    create ()
+    {
+      
+        //this.add.image(400, 300, 'background');
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.player = this.physics.add.image(400, 300, 'ship');
+        this.player.setScale(0.1);
+        
+        this.player.setCollideWorldBounds(true);
+    }
+
+    update ()
+    {
+        this.player.setVelocity(0);
+
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-300);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(300);
+        }
+
+        if (this.cursors.up.isDown)
+        {
+            this.player.setVelocityY(-300);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.setVelocityY(300);
+        }
+    }
+
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game-container',
+    transparent: true,
+    width: 800,
+    height: 600,
+    disableContextMenu: false,
+    scene: KeyboardExample, 
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    }
+};
+
+
+const game = new Phaser.Game(config);
